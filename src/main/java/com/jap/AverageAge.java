2fc8f05aa7180d6e368a8b6d957f74ed9a03684f
Sangti konyak <ssangtikonyak@gmail.com>
2022-10-16 17:33:34 +0530

    commit 1

--- src/main/java/com/jap/AverageAge.java
+++ src/main/java/com/jap/AverageAge.java
@@ -1,22 +1,30 @@
 package com.jap;
 
+import java.util.Arrays;
+
 public class AverageAge {
 
     public String averageAgeCalculator(String[] ageRawData)
     {
-        double avg = 0;
+        double avg=0;
+        double total=0;
 
         try{
-            //Write the logic
-
-            //Handle specific exception
-        }catch (Exception exception)
+            for(int i=0;i<ageRawData.length;i++)
+            {
+                total+=Integer.parseInt(ageRawData[i]);
+            }
+            avg=total/ageRawData.length;
+        }catch (NumberFormatException e)
         {
-            return exception.toString();
+            return e.toString();
         }
         return String.valueOf(avg);
     }
 
-
+    public static void main(String[] args) {
+        AverageAge averageAge=new AverageAge();
+        System.out.println("averageAge.averageAgeCalculator(new String[]{\"33\",\"22\"}) = " + averageAge.averageAgeCalculator(new String[]{"33", "22"}));
+    }
 
 }
